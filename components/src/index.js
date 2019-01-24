a41bbf54772e02d646e90576651a3cc7c69a40b7
Jordan Stoddard <jjsincorporated@gmail.com>
2019-01-24 15:53:46 -0800

    added favicon.ico

---
+++ components/src/index.js
@@ -0,0 +1,13 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+
+const App = () => {
+return (
+    
+<div>Hi there!</div>
+
+)
+}
+
+ReactDOM.render(<App/>, document.querySelector('#root'))
