a16ab70d0383930f56cd45f9f98a798e34bdd793
Jordan Stoddard <jjsincorporated@gmail.com>
2019-01-24 16:09:02 -0800

    added JSX for first comment display

--- components/src/index.js
+++ components/src/index.js
@@ -1,13 +1,25 @@
-import ReactDOM from 'react-dom'
-import React from 'react'
-
+import ReactDOM from "react-dom";
+import React from "react";
 
 const App = () => {
-return (
-    
-<div>Hi there!</div>
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img src="" alt="image of user" title="image of user" />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date">{Date.now()}</span>
+          </div>
+          <div className="text">Nice blog post!</div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-)
-}
-
-ReactDOM.render(<App/>, document.querySelector('#root'))
+ReactDOM.render(<App />, document.querySelector("#root"));
