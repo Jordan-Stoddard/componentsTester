805352712d9d3704e51b1af918d4ab2858245507
Jordan Stoddard <jjsincorporated@gmail.com>
2019-01-26 14:41:39 -0800

    Put comment details inside Approval Card

--- components/src/index.js
+++ components/src/index.js
@@ -2,28 +2,39 @@ import ReactDOM from "react-dom";
 import React from "react";
 import faker from "faker";
 import CommentDetail from "./CommentDetail.jsx";
+import ApprovalCard from "./ApprovalCard.jsx";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        name={faker.name.firstName()}
-        time="Today at 6:00PM"
-        phrase={faker.hacker.phrase()}
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        name={faker.name.firstName()}
-        time="Today at 6:15PM"
-        phrase={faker.hacker.phrase()}
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        name={faker.name.firstName()}
-        time="Today at 6:45PM"
-        phrase={faker.hacker.phrase()}
-        avatar={faker.image.avatar()}
-      />
+    
+      <ApprovalCard>
+        <CommentDetail
+          name={faker.name.firstName()}
+          time="Today at 6:15PM"
+          phrase={faker.hacker.phrase()}
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          name={faker.name.firstName()}
+          time="Today at 6:45PM"
+          phrase={faker.hacker.phrase()}
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          name={faker.name.firstName()}
+          time="Today at 6:00PM"
+          phrase={faker.hacker.phrase()}
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
     </div>
   );
 };
